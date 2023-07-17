d5d8d42104f320ec05deb63a98603a865f56fdde
sjpetrick <sjpetrick@gmail.com>
2023-07-17 05:18:50 +0000

    feat: add username input response

--- number_guess.sh
+++ number_guess.sh
@@ -6,3 +6,13 @@ SECRET_NUMBER=$(( 1 + $RANDOM % 1000 ))
 
 echo "Enter your username:"
 read USERNAME
+
+
+if [ "$($PSQL "SELECT EXISTS (SELECT 1 FROM users WHERE username = '$USERNAME')")" = "t" ]
+then
+  GAMES_PLAYED=$($PQSL "SELECT COUNT game_id FROM games WHERE username = '$USERNAME'")
+  BEST_GAME=$($PSQL "SELECT MIN(score) FROM games WHERE username = '$USERNAME'")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+else
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
