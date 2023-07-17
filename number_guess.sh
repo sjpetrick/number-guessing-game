ce3418c53b45bfeefb6d98682e8cdea0fd7cbc74
sjpetrick <sjpetrick@gmail.com>
2023-07-17 06:08:28 +0000

    feat: add guess comparison

--- number_guess.sh
+++ number_guess.sh
@@ -26,3 +26,18 @@ do
   echo "That is not an integer, guess again:"
   read GUESS
 done
+
+SCORE=0
+while [ $GUESS !=  $SECRET_NUMBER ]
+do
+  if [ $GUESS -lt $SECRET_NUMBER ]
+  then 
+    echo "It's higher than that, guess again:"
+  else
+    echo "It's lower than that, guess again:"
+  fi
+  read GUESS
+  ((SCORE=SCORE+1))
+done 
+((SCORE=SCORE+1))
+echo "You guessed it in $SCORE tries. The secret number was $SECRET_NUMBER. Nice job!"
